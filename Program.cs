9ce73300305945a6e4241a753cb29e5f9aecce10
Timasuper05 <timasuper05@yandex.ru>
2026-09-12 19:04:45 +0300

    Время по Махачкале, gitignore

--- Program.cs
+++ Program.cs
@@ -1,19 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
-HttpClient httpClient = new HttpClient();
-httpClient.DefaultRequestHeaders.Accept.Clear();
-httpClient.DefaultRequestHeaders.Accept.Add(
-    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-await GetGeoLocationAsync(httpClient);
-static async Task GetGeoLocationAsync(HttpClient client)
-{
-    string geocodingUrl = "https://geocoding-api.open-meteo.com/v1/search?name={Makhachkala}&count=1&language=en";
-    var json = await client.GetStringAsync(geocodingUrl);
-
-    Console.Write(json);
-}
+Console.WriteLine("Time:");
+DateTime timeZoneMoscow = TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time"));
+Console.WriteLine(timeZoneMoscow);

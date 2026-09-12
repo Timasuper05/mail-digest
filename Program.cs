e5393d776e54436c9a77ff8908d6f37415cdd5f8
Timasuper05 <timasuper05@yandex.ru>
2026-09-12 22:40:08 +0300

    Список писем за сутки через IMAP

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-﻿using System.Net.Http.Headers;
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using System.Net.Http.Headers;
 using System.Text.Json;
-
-Console.WriteLine("Time:");
+string mailLog = Environment.GetEnvironmentVariable("MAIL_LOGIN");
+string mailPas = Environment.GetEnvironmentVariable("MAIL_PASSWORD");
+using var client = new ImapClient();
+await client.ConnectAsync("imap.yandex.ru", 993, MailKit.Security.SecureSocketOptions.Auto);
+if (client.IsConnected)
+{
+    await client.AuthenticateAsync(mailLog, mailPas);
+}
 DateTime timeZoneMoscow = TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time"));
-Console.WriteLine(timeZoneMoscow);
+client.Inbox.Open(FolderAccess.ReadOnly);
+DateTime date = timeZoneMoscow.AddDays(-1);
+var a = client.Inbox.Search(SearchQuery.DeliveredAfter(date));
+var s = client.Inbox.Fetch(a, MessageSummaryItems.Envelope);
+
+foreach (var item in  s)
+{
+    
+        Console.WriteLine($"Входящие: {item.Envelope.From} {item.Envelope.Subject} {item.Date} ");
+}
